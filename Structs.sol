c6d0f81ba39fff4e104969d7550c9a15ef0c7ecb
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-10-31 12:29:22 -0400

    structs

---
+++ Structs.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract Struct {
+    
+enum Status {
+    TODO,
+    IN_PROGRESS,
+    DONE,
+    CANCELLED
+}
+
+struct Task {
+    string title;
+    string description;
+    Status status;
+}
+mapping(uint256 => Task) tasks;
+
+function deleteTask( uint256 id) public {
+    delete tasks[id];
+}
+function editTaskTitle(uint256 id, string memory newTitle) public {
+    tasks[id].title = newTitle;
+}
+function addTask(uint256 id, string memory title, string memory description) public {
+    tasks[id] = Task({
+        title: title,
+        description: description,
+        status: Status.TODO
+    });
+}
+}
