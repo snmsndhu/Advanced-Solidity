0d15cb0c084717c21c0531085b9e8ffb3168e9c4
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-10-31 12:01:37 -0400

    update

--- Enums.sol
+++ Enums.sol
@@ -2,21 +2,42 @@
 pragma solidity ^0.8.4;
 
 // Normal Approach 
-contract Normal {
-mapping(uint256 => string) todos;
+// contract Normal {
+// mapping(uint256 => string) todos;
+
+// function addTask(uint256 id) public {
+//     todos[id] ="ToDo";
+// }
+
+// function updateStatus(uint256 id, string memory newStatus) public {
+//     todos[id] = newStatus;
+// }
+
+// function getStatus(uint256 id) public view returns (string memory) {
+//     return todos[id];
+// }
+// }
+
+// Lets do it with Enums.
+contract Ennums {
+enum Status {
+    TODO,
+    IN_PROGRESS,
+    DONE,
+    CANCELLED
+}
+
+mapping(uint256 => Status) todos;
 
 function addTask(uint256 id) public {
-    todos[id] ="ToDo";
+    todos[id] = Status.TODO;
 }
 
-function updateStatus(uint256 id, string memory newStatus) public {
-    todos[id] = newStatus;
+function markTaskInProgress(uint256 id) public {
+    todos[id] = Status.IN_PROGRESS;
 }
 
-function getStatus(uint256 id) public view returns (string memory) {
+function getStatus(uint256 id) public view returns (Status){
     return todos[id];
 }
 }
-
-// Lets do it with Enums.
-
